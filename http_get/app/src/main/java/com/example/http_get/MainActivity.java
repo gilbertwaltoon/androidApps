aa89b044763fab41c6a2a7dabb1b2973a017ea36
gilbertwaltoon <gilbertwaltoon@gmail.com>
2021-09-06 17:02:49 +0100

    Added http_get

---
+++ http_get/app/src/main/java/com/example/http_get/MainActivity.java
@@ -0,0 +1,123 @@
+package com.example.http_get;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+// import android.accessibilityservice.AccessibilityService;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkError;
+import com.android.volley.NoConnectionError;
+import com.android.volley.ParseError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.ServerError;
+import com.android.volley.TimeoutError;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.apache.http.conn.ConnectTimeoutException;
+import org.json.JSONException;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.net.ConnectException;
+import java.net.MalformedURLException;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView textView = (TextView) findViewById(R.id.text);
+// ...
+
+// Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://www.google.com";
+       // String url = "http://neverssl";
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        textView.setText("Response is: " + response.substring(0, 500));
+                        Toast.makeText(getApplicationContext(), "Got an answer!",
+                                Toast.LENGTH_LONG).show();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText("That didn't work!");
+                if (error instanceof NoConnectionError) {
+                    Context context = getApplicationContext();
+                    ConnectivityManager cm = (ConnectivityManager) context
+                            .getSystemService(Context.CONNECTIVITY_SERVICE);
+                    NetworkInfo activeNetwork = null;
+                    if (cm != null) {
+                        activeNetwork = cm.getActiveNetworkInfo();
+                    }
+                    if (activeNetwork != null && activeNetwork.isConnectedOrConnecting()) {
+                        Toast.makeText(getApplicationContext(), "Server is not connected to internet.",
+                                Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Your device is not connected to internet.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                } else if (error instanceof NetworkError || error.getCause() instanceof ConnectException
+                        || (error.getCause().getMessage() != null
+                        && error.getCause().getMessage().contains("connection"))) {
+                    Toast.makeText(getApplicationContext(), "Your device is not connected to internet.",
+                            Toast.LENGTH_SHORT).show();
+                } else if (error.getCause() instanceof MalformedURLException) {
+                    Toast.makeText(getApplicationContext(), "Bad Request.", Toast.LENGTH_SHORT).show();
+                } else if (error instanceof ParseError || error.getCause() instanceof IllegalStateException
+                        || error.getCause() instanceof JSONException
+                        || error.getCause() instanceof XmlPullParserException) {
+                    Toast.makeText(getApplicationContext(), "Parse Error (because of invalid json or xml).",
+                            Toast.LENGTH_SHORT).show();
+                } else if (error.getCause() instanceof OutOfMemoryError) {
+                    Toast.makeText(getApplicationContext(), "Out Of Memory Error.", Toast.LENGTH_SHORT).show();
+                } else if (error instanceof AuthFailureError) {
+                    Toast.makeText(getApplicationContext(), "server couldn't find the authenticated request.",
+                            Toast.LENGTH_SHORT).show();
+                } else if (error instanceof ServerError || error.getCause() instanceof ServerError) {
+                    Toast.makeText(getApplicationContext(), "Server is not responding.", Toast.LENGTH_SHORT).show();
+                } else if (error instanceof TimeoutError || error.getCause() instanceof SocketTimeoutException
+                        || error.getCause() instanceof ConnectTimeoutException
+                        || error.getCause() instanceof SocketException
+                        || (error.getCause().getMessage() != null
+                        && error.getCause().getMessage().contains("Connection timed out"))) {
+                    Toast.makeText(getApplicationContext(), "Connection timeout error",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "An unknown error occurred.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+
+
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+        Toast.makeText(getApplicationContext(), "Done queue.add(stringRequest).",
+                Toast.LENGTH_SHORT).show();
+
+
+    }
+}
+
